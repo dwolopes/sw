e85571a59f446cf32f1212c7cd02d61e910ac9b6
Lopes <douglas.lopes@cromg.org.br>
2018-07-04 12:14:54 -0300

    comentários de ajuda calala

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -17,21 +17,69 @@ IndexController.prototype._registerServiceWorker = function() {
   var indexController = this;
 
   navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    // Udacity hint
     // TODO: if there's no controller, this page wasn't loaded
     // via a service worker, so they're looking at the latest version.
     // In that case, exit early
 
+    // Douglas' Comments
+    //Eu não entendi pq temos que verificar se o arquivo é controlado ou não por um sw =/
+    if(!navigator.serviceWorker.controller){
+      return
+    }
+
+    // Udacity hint
     // TODO: if there's an updated worker already waiting, call
     // indexController._updateReady()
+    if(reg.waiting){
+      // there's an update ready!
+      indexController._updateReady()
+      return;
+    }
 
+    // Udacity hint
     // TODO: if there's an updated worker installing, track its
     // progress. If it becomes "installed", call
     // indexController._updateReady()
+    if(reg.installing){
+      // there's an uptade in progress
+      reg.installing.addEventListener('statechange', function(){
+        /**
+         * Douglas' Comments
+         * Qual é o contexto desse 'this' IndezController ou reg ? Eu tentei dar um console.log 
+         * nesse this, porém é como se meu sw nunca passasse por essa fase, desse modo não conseguo ver o console.
+         * Eu imaginei que eu deveria utilizar arraw funtion para garantir que o this se refere a reg, mas não tenho
+         * certeza. Não sei se o this de 'this.state' é realmente o objeto de reg.installing
+         */
+        if(this.state == 'installed'){
+          indexController._updateReady()
+        }
+      })
+    }
 
+    // Udacity hint
     // TODO: otherwise, listen for new installing workers arriving.
     // If one arrives, track its progress.
     // If it becomes "installed", call
     // indexController._updateReady()
+    /**
+     * Também tenho dúvida em relação ao this abaixo, e o grande problemaque é se minha app não identificasse
+     * as atualizações que faço no sw. Desse modo não consigo acessar esse evento e ver o contexto do this.
+     * Eu tinha em mente que com function, o this tem o contexto de quem o chama, então nesse caso seria reg, pois 
+     * chamamos a função anônima como parâmetro de addEventListener de reg. Está correto pensar assim?
+     */
+    reg.addEventListener('uptadefound', function() {
+      reg.installing.addEventListener('statechange', function() {
+        if(this.state == 'installed'){
+          indexController._updateReady()
+        }
+      })
+    })
+    /**
+     * Por fim eu fiz os testes do códido e segundo o sisteminha de testes oferecido no curso, eu conseguir completar 
+     * a tarefa. Porém não entendi, por qual motivo não consigo logar os 'this' acima, mesmo atualizando e instalando 
+     * os estados de reg no arquivo do sw :(
+     */
   });
 };
 
